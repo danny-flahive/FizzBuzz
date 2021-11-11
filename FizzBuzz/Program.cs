3a1e99e75b110ea3b5a9379d9772ecba04200e0a
danny-flahive <93837720+danny-flahive@users.noreply.github.com>
2021-11-11 17:04:56 +0000

    Added further functionality
    Added two extra rules (7 and 11).  The user can now determine which rules to apply via command line arguments. The user is asked to determine the value FizzBuzz is run up to.

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace FizzBuzz
 {
@@ -6,16 +7,27 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 100; i++)
+            var rules = (args.Length == 0) ? new int[] { 3, 5, 7, 11, 13, 17 } : args.Select(p => int.Parse(p));
+            Console.Write("Enter a maximum value for FizzBuzz: ");
+            int maxValue = int.Parse(Console.ReadLine());
+            for (int i = 0; i < maxValue; i++)
             {
                 string output = "";
-                if (i % 3 == 0)
+                if (i % 3 == 0 && rules.Contains(3))
                 {
                     output += "Fizz";
                 }
-                if (i % 5 == 0)
+                if (i % 5 == 0 && rules.Contains(5))
                 {
                     output += "Buzz";
+                }
+                if (i % 7 == 0 && rules.Contains(7))
+                {
+                    output += "Bang";
+                }
+                if (i % 11 == 0 && rules.Contains(11))
+                {
+                    output = "Bong";
                 }
                 if (output == "")
                 {
